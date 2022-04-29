0b2d3e532d68e3f39eba9c89dac62fd31ad869a5
Dmitry <dimasokolov2002g@gmail.com>
2022-04-29 23:40:07 +0300

    Добавлены страницы курса.

---
+++ Smart-book/Step2.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Smart_book
+{
+    /// <summary>
+    /// Логика взаимодействия для Step2.xaml
+    /// </summary>
+    public partial class Step2 : Page
+    {
+        public Step2()
+        {
+            InitializeComponent();
+        }
+    }
+}
